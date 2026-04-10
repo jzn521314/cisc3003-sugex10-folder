230134be8f354b42925faf4e6e1884cc4d2c9ec0
jzn521314 <923076663@qq.com>
2026-04-10 22:53:55 +0800

    Refactor HTML structure and improve layout

--- cisc3003-sugex10.php
+++ cisc3003-sugex10.php
@@ -52,13 +52,51 @@ $selectedID = isset($_GET['id']) ? $_GET['id'] : null;
   </div>
 </header>
 
-<!-- LEFT -->
-<div class="mdl-layout__drawer mdl-color--blue-grey-800 mdl-color-text--white">
+<!-- LEFT（已修复） -->
+<div class="mdl-layout__drawer mdl-color--blue-grey-800 mdl-color-text--blue-grey-50">
+
    <div class="profile">
        <img src="images/profile.jpg" class="avatar" alt="profile">
        <h2>John Locke</h2>
        <span>johnlocke@example.com</span>
    </div>
+
+   <nav class="mdl-navigation mdl-color-text--blue-grey-300">
+
+       <a class="mdl-navigation__link mdl-color-text--blue-grey-300" href="#">
+           <i class="material-icons">dashboard</i> Dashboard
+       </a>
+
+       <a class="mdl-navigation__link mdl-color-text--blue-grey-300" href="#">
+           <i class="material-icons">message</i> Messages
+       </a>
+
+       <a class="mdl-navigation__link mdl-color-text--blue-grey-300" href="#">
+           <i class="material-icons">event</i> Tasks
+       </a>
+
+       <a class="mdl-navigation__link mdl-color-text--blue-grey-300" href="#">
+           <i class="material-icons">call</i> Orders
+       </a>
+
+       <a class="mdl-navigation__link mdl-color-text--blue-grey-300" href="#">
+           <i class="material-icons">settings</i> Configure
+       </a>
+
+       <a class="mdl-navigation__link mdl-color-text--blue-grey-300" href="#">
+           <i class="material-icons">view_list</i> Catalog
+       </a>
+
+       <a class="mdl-navigation__link mdl-color-text--blue-grey-300" href="#">
+           <i class="material-icons">contacts</i> Customers
+       </a>
+
+       <a class="mdl-navigation__link mdl-color-text--blue-grey-300" href="#">
+           <i class="material-icons">insert_chart</i> Analytics
+       </a>
+
+   </nav>
+
 </div>
 
 <main class="mdl-layout__content">
